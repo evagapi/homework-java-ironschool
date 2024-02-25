e54579ec59c6f23cb3b5e5f377979d972f1c544d
evagapi <87602505+evagapi@users.noreply.github.com>
2024-02-25 22:58:28 +0100

    feat: add getTotalProfit to School class

--- src/main/java/model/School.java
+++ src/main/java/model/School.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
@@ -15,13 +16,48 @@ public class School {
     @Setter(AccessLevel.NONE)
     private final String id;
     private String name;
-    private HashMap<String, Teacher> teachers;
-    private HashMap<String, Course> courses;
-    private HashMap<String, Student> students;
-    private double totalProfit;
+    private HashMap<String, Teacher> teachers = new HashMap<>();
+    private HashMap<String, Course> courses = new HashMap<>();
+    private HashMap<String, Student> students = new HashMap<>();
 
     public School(String name) {
         id = UUID.randomUUID().toString();
         this.name = name;
+    }
+
+    public void addCourse(Course course) {
+        courses.put(course.getCourseId(), course);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.put(teacher.getTeacherId(), teacher);
+    }
+
+    public void addStudent(Student student) {
+        students.put(student.getStudentId(), student);
+    }
+
+    public double getTotalProfit() {
+        return getTotalEarned() - computeSalaries();
+    }
+
+    private double computeSalaries() {
+        double totalSpent = 0;
+        for (Map.Entry<String, Teacher> entry : teachers.entrySet()) {
+            Teacher teacher = entry.getValue();
+            double spent = teacher.getSalary();
+            totalSpent += spent;
+        }
+        return totalSpent;
+    }
+
+    private double getTotalEarned() {
+        double totalEarned = 0;
+        for (Map.Entry<String, Course> entry : courses.entrySet()) {
+            Course course = entry.getValue();
+            double earned = course.getMoney_earned();
+            totalEarned += earned;
+        }
+        return totalEarned;
     }
 }
